e02661d79bc3ea6f27f66a0b72c6ab03dcc65fe0
colfab98 <colantoniofabio98@gmail.com>
2025-07-09 09:38:32 +0200

    added neighbors_list

--- plasgraph/engine.py
+++ plasgraph/engine.py
@@ -35,16 +35,18 @@ def objective(trial, accelerator, parameters, data, splits, labeled_indices):
     trial.set_user_attr("pid", os.getpid())
 
     trial_params_dict = parameters._params.copy()
-    trial_params_dict['l2_reg'] = trial.suggest_float("l2_reg", 1e-8, 1e-6, log=True)
-    trial_params_dict['n_channels'] = trial.suggest_int("n_channels", 210, 310, step=16)
-    trial_params_dict['n_gnn_layers'] = trial.suggest_int("n_gnn_layers", 8, 12)
-    trial_params_dict['dropout_rate'] = trial.suggest_float("dropout_rate", 0.1, 0.4)
+    trial_params_dict['l2_reg'] = trial.suggest_float("l2_reg", 1e-6, 1e-2, log=True)
+    trial_params_dict['n_channels'] = trial.suggest_int("n_channels", 16, 128, step=16)
+    trial_params_dict['n_gnn_layers'] = trial.suggest_int("n_gnn_layers", 4, 6)
+    trial_params_dict['dropout_rate'] = trial.suggest_float("dropout_rate", 0.0, 0.3)
     trial_params_dict['gradient_clipping'] = trial.suggest_float("gradient_clipping", 0.0, 0.3)
-    trial_params_dict['edge_gate_hidden_dim'] = trial.suggest_int("edge_gate_hidden_dim", 64, 120, step=8)
-    trial_params_dict['n_channels_preproc'] = trial.suggest_int("n_channels_preproc", 20, 50, step=5)
+    trial_params_dict['edge_gate_hidden_dim'] = trial.suggest_int("edge_gate_hidden_dim", 16, 64, step=8)
+    trial_params_dict['n_channels_preproc'] = trial.suggest_int("n_channels_preproc", 5, 20, step=5)
     trial_params_dict['edge_gate_depth'] = trial.suggest_int("edge_gate_depth", 2, 6)
     trial_params_dict['batch_size'] = trial.suggest_categorical('batch_size', [512, 1024, 2048])
 
+    n_layers = trial_params_dict['n_gnn_layers']
+    neighbors_list = [15] + [10] * (n_layers - 1)
     
     trial_config_obj = config.config(parameters.config_file_path)
     trial_config_obj._params = trial_params_dict
@@ -70,7 +72,7 @@ def objective(trial, accelerator, parameters, data, splits, labeled_indices):
         train_loader = NeighborLoader(
             data,
             input_nodes=torch.from_numpy(train_fold_indices).to(data.x.device),
-            num_neighbors=[-1], # Use all neighbors. Can be tuned e.g., [15, 10]
+            num_neighbors=neighbors_list, # Use all neighbors. Can be tuned e.g., [15, 10]
             shuffle=True,
             # Use the new tunable batch_size and num_workers from config
             batch_size=trial_config_obj['batch_size'],
@@ -81,7 +83,7 @@ def objective(trial, accelerator, parameters, data, splits, labeled_indices):
         val_loader = NeighborLoader(
             data,
             input_nodes=torch.from_numpy(val_fold_indices).to(data.x.device),
-            num_neighbors=[-1],
+            num_neighbors=neighbors_list,
             shuffle=False,
             batch_size=trial_config_obj['batch_size'],
             num_workers=trial_config_obj['num_workers'],
--- tune_hpo.sh
+++ tune_hpo.sh
@@ -5,10 +5,12 @@
 #SBATCH --gpus=4
 #SBATCH --time=04:00:00
 
+export TOKENIZERS_PARALLELISM=false
+
 PYTHONUNBUFFERED=1 accelerate launch --num_processes=4 --mixed_precision=fp16 -m scripts.tune_hpo \
     --data_cache_dir cache/eskapee-train/ \
     plasgraph_config.yaml \
-    plasgraph2-datasets/eskapee-train_small.csv \
+    plasgraph2-datasets/eskapee-train.csv \
     plasgraph2-datasets/ \
     output/ESKAPEE_hpo_study \
     > output/hpo.log 2> output/hpo.err
